31e0797899caf88e7ad8904adf2646ecbaf05ca3
Ali Ragab <alighonim78@gmail.com>
2026-04-17 23:44:47 +0200

    user/validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  
+  validates :name, presence: true, length: { in: 2..50 }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :dob, presence: true 
+  validates :phone, presence: true, numericality: { only_integer: true }, length: { in: 10..15 }
 end
