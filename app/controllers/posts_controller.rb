94d96ea6be6e093b6f79bf32257e6b0cf627062c
Ali Ragab <alighonim78@gmail.com>
2026-04-17 23:53:24 +0200

    post/destroy&comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,56 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
 
- def index
-  @posts = Post.all
- end
+  # GET /posts
+  def index
+    @posts = Post.all
+  end
 
-def show
-end
+  # GET /posts/:id
+  def show
+  end
 
-def new
-  @post = Post.new
-end
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
 
+  # POST /posts
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
 
-end
+  # GET /posts/:id/edit
+  def edit
+  end
 
+  # PATCH/PUT /posts/:id
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post was successfully updated.'
+    else
+      render :edit
+    end
+  end
 
+  # DELETE /posts/:id
+  def destroy
+    @post.destroy
+    redirect_to posts_url, notice: 'Post was successfully destroyed.'
+  end
 
-def set_post
-  @post = Post.find(params[:id])
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
