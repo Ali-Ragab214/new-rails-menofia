f1f1a1153499bd311764723699cbec962a91b76e
Ali Ragab <alighonim78@gmail.com>
2026-04-18 00:12:39 +0200

    user#new&create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,25 @@ before_action:set_user,only %[show edit update destroy]
 def index
   @users=User.all
 end
-
+# GET /users/:id
 def show
 end
+
+# GET /users/new
+def new
+  @user=User.new
+end
+# POST /users
+def create
+  @user =User.new(user_params)
+  if @user.save
+    redirect_to @user, notice: "User was successfully created."
+  else
+    render :new
+  end
+end
+
+
 
   def set_user
     @user=User.find(params[:id])
