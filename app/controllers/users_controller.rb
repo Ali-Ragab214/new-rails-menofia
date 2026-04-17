5dfa2a31b6d451bb96aa9e7991182ac2dad4417b
Ali Ragab <alighonim78@gmail.com>
2026-04-18 00:09:27 +0200

    user / set=>user & params functions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,15 @@
 class UsersController < ApplicationController
-  # your code goes here
+before_action:set_user,only %[show edit update destroy]
+
+
+
+
+  def set_user
+    @user=User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.require("user").permit(:name, :email, :dob, :phone)
+  end
 end
